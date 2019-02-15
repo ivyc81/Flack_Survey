5d60cd1d9389d86535e9dc71a7997c585e5eb947
Gabriela <balicas.gb@gmail.com>
2019-02-14 16:46:45 -0800

    updated form presentation

--- app.py
+++ app.py
@@ -9,14 +9,24 @@ debug = DebugToolbarExtension(app)
 
 surveys["satisfaction"]
 
+
 @app.route("/")
 def show_start_survey():
     """ creates the satisfaction survey form"""
 
-    return render_template("landing.html", survey_title = surveys["satisfaction"].title, survey_instruction = surveys["satisfaction"].instructions)
+    session['responses'] = []
 
-@app.route("/", methods=["POST"])
-def show_satisfaction_survey():
-    """ creates the satisfaction survey form"""
+    return render_template("landing.html", survey_title=surveys["satisfaction"].title, survey_instruction=surveys["satisfaction"].instructions)
 
-    return render_template("landing.html", survey_title = surveys["satisfaction"].title, survey_instruction = surveys["satisfaction"].instructions)
+
+@app.route("/questions/<int:question_number>", methods=["POST"])
+def show_satisfaction_survey(question_number):
+    """ creates the satisfaction survey form """
+
+    question = surveys["satisfaction"].questions[question_number]
+
+    question_string_list = [question.question, question.choices[0], question.choices[1]]
+
+    next_question = question_number + 1
+
+    return render_template("form.html", survey_title=surveys["satisfaction"].title, survey_instruction=surveys["satisfaction"].instructions, question=question_string_list, next_question = next_question)
